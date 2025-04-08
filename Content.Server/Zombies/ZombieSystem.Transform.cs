1b8dff48131bd064eec7d6a029e0df359e528919
AndrewFenriz <78079974+AndrewFenriz@users.noreply.github.com>
2025-04-08 03:15:23 +0300

    Update Content.Server/Zombies/ZombieSystem.Transform.cs
    Co-authored-by: qrtDaniil <58003418+qrtDaniil@users.noreply.github.com>

--- Content.Server/Zombies/ZombieSystem.Transform.cs
+++ Content.Server/Zombies/ZombieSystem.Transform.cs
@@ -200,11 +200,11 @@ public sealed partial class ZombieSystem
         //Should prevent instances of zombies using comms for information they shouldnt be able to have.
         _inventory.TryUnequip(target, "ears", true, true);
         //Needed to deprive a zombie of a jetpack.
-        _inventory.TryUnequip(target, "back", true, true);
+        _inventory.TryUnequip(target, "back", true, true); # DS14
         //Also needed to deprive zombies of their jetpack.
-        _inventory.TryUnequip(target, "belt", true, true);
+        _inventory.TryUnequip(target, "belt", true, true); # DS14
         //And this is also needed to deprive the zombie of a jetpack.
-        _inventory.TryUnequip(target, "suitstorage", true, true);
+        _inventory.TryUnequip(target, "suitstorage", true, true); # DS14
 
         //popup
         _popup.PopupEntity(Loc.GetString("zombie-transform", ("target", target)), target, PopupType.LargeCaution);
