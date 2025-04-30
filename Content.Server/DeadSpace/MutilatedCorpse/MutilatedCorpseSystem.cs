fd959e124abb795b1d843136182a0d5fcf661b15
21Melkuu <79728504+21Melkuu@users.noreply.github.com>
2025-05-01 01:07:37 +0300

    using

--- Content.Server/DeadSpace/MutilatedCorpse/MutilatedCorpseSystem.cs
+++ Content.Server/DeadSpace/MutilatedCorpse/MutilatedCorpseSystem.cs
@@ -1,11 +1,9 @@
 // Мёртвый Космос, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/dead-space-server/space-station-14-fobos/master/LICENSE.TXT
 
-using System.IO;
 using Content.Shared.Damage;
 using Content.Shared.IdentityManagement;
 using Content.Shared.IdentityManagement.Components;
 using Content.Shared.Mobs.Systems;
-using Robust.Shared.Player;
 
 namespace Content.Server.DeadSpace.MutilatedCorpse;
 
