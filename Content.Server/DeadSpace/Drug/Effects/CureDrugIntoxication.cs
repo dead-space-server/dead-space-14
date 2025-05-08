df1ec69461f41a684927dee9db4460829a8fd980
KloopRE <100780612+KloopRE@users.noreply.github.com>
2025-05-08 17:13:57 +0300

    Update CureDrugIntoxication.cs

--- Content.Server/DeadSpace/Drug/Effects/CureDrugIntoxication.cs
+++ Content.Server/DeadSpace/Drug/Effects/CureDrugIntoxication.cs
@@ -1,7 +1,6 @@
 // Мёртвый Космос, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/dead-space-server/space-station-14-fobos/master/LICENSE.TXT
 
 using Content.Server.DeadSpace.Drug.Components;
-using Content.Server.DeadSpace.Drug;
 using Content.Shared.EntityEffects;
 using Robust.Shared.Prototypes;
 
