a73ef9e22a717d610e84d39562d99c95a7fd749d
KloopRE <100780612+KloopRE@users.noreply.github.com>
2025-07-02 20:22:55 +0300

    Update LimitedChargesComponent.cs

--- Content.Shared/Charges/Components/LimitedChargesComponent.cs
+++ Content.Shared/Charges/Components/LimitedChargesComponent.cs
@@ -8,12 +8,13 @@ namespace Content.Shared.Charges.Components;
 [AutoGenerateComponentState]
 public sealed partial class LimitedChargesComponent : Component
 {
-    /// DeadSpace
+    // DS14-start
     public LimitedChargesComponent(int max, int count)
     {
         Charges = count;
         MaxCharges = max;
     }
+    // DS14-end
 
     /// <summary>
     /// The maximum number of charges
