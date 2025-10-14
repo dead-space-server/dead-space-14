1e79fc5d16ab7d23acd3c99c0b929ddbe96924ca
qrtDaniil <58003418+qrtDaniil@users.noreply.github.com>
2025-10-14 17:09:28 +0300

    fix

--- Content.Server/Revenant/EntitySystems/TelepathySystem.cs
+++ Content.Server/Revenant/EntitySystems/TelepathySystem.cs
@@ -52,7 +52,7 @@ public sealed partial class TelepathySystem : EntitySystem
                         if (TryComp<LanguageComponent>(args.User, out var language))
                             lexiconMessage = _language.ReplaceWordsWithLexicon(message, language.SelectedLanguage);
 
-                        var selectedLanguage = language != null ? language.SelectedLanguage.Id : _language.GetDefaultLanguageId();
+                        var selectedLanguage = language != null ? language.SelectedLanguage : LanguageSystem.DefaultLanguageId;
 
                         _prayerSystem.SendSubtleMessage(targetActor.PlayerSession, player, message, Loc.GetString("prayer-popup-subtle-revenant"));
                         var ev = new EntitySpokeToEntityEvent(args.Target, message, lexiconMessage, selectedLanguage);
