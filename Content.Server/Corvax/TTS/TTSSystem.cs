aff1f4ec74de70a66554bf723cbe8d7c5f0a839f
KloopRE <100780612+KloopRE@users.noreply.github.com>
2025-10-20 00:02:00 +0300

    Update TTSSystem.cs

--- Content.Server/Corvax/TTS/TTSSystem.cs
+++ Content.Server/Corvax/TTS/TTSSystem.cs
@@ -243,12 +243,12 @@ public sealed partial class TTSSystem : EntitySystem
                 if (!understanding.Contains(session))
                 {
                     if (soundLexiconData is null)
-                        RaiseNetworkEvent(new PlayTTSEvent(new byte[0], GetNetEntity(uid), isWhisper: true, isSoundLexicon: true, languageId: languageId), session);
+                        RaiseNetworkEvent(new PlayTTSEvent(new byte[0], GetNetEntity(uid), isWhisper: true, isRadio: true, isSoundLexicon: true, languageId: languageId), session);
                     else
-                        RaiseNetworkEvent(new PlayTTSEvent(soundLexiconData, GetNetEntity(uid), isWhisper: true), session);
+                        RaiseNetworkEvent(new PlayTTSEvent(soundLexiconData, GetNetEntity(uid), isWhisper: true, isRadio: true), session);
                 }
                 else
-                    RaiseNetworkEvent(new PlayTTSEvent(soundData, GetNetEntity(uid), isWhisper: true), session);
+                    RaiseNetworkEvent(new PlayTTSEvent(soundData, GetNetEntity(uid), isWhisper: true, isRadio: true), session);
             }
         }
     }
