9712d42d7c9a13903784a3fb56ed61feb4104291
AndrewFenriz <78079974+AndrewFenriz@users.noreply.github.com>
2025-10-21 19:27:07 +0300

    Update Content.Server/Research/Systems/ResearchSystem.cs
    Co-authored-by: qrtDaniil <58003418+qrtDaniil@users.noreply.github.com>

--- Content.Server/Research/Systems/ResearchSystem.cs
+++ Content.Server/Research/Systems/ResearchSystem.cs
@@ -46,7 +46,7 @@ namespace Content.Server.Research.Systems
         /// <param name="serverUid"></param>
         /// <param name="serverComponent"></param>
         /// <returns></returns>
-        // DS14-start
+        // DS14-edit-start
         public HashSet<Entity<ResearchServerComponent>> GetServers(EntityUid client)
         {
             ClientLookup.Clear();
