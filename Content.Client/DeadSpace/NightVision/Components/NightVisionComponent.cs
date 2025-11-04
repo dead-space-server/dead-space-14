f6594dfdfa66eb825b61d029f223b28e213723d6
KloopRE <100780612+KloopRE@users.noreply.github.com>
2025-11-04 13:19:08 +0300

    лишнее

--- Content.Client/DeadSpace/NightVision/Components/NightVisionComponent.cs
+++ Content.Client/DeadSpace/NightVision/Components/NightVisionComponent.cs
@@ -6,19 +6,6 @@ namespace Content.Client.DeadSpace.Components.NightVision;
 public sealed partial class NightVisionComponent : SharedNightVisionComponent
 {
     /// <description>
-    ///     Used to ensure that this doesn't break with sandbox or admin tools.
-    ///     This is not "enabled/disabled".
-    /// </description>
-    [Access(Other = AccessPermissions.ReadWriteExecute)]
-    public bool LightSetup = false;
-
-    /// <description>
-    ///     Gives an extra frame of nighyvision to reenable light manager during
-    /// </description>
-    [Access(Other = AccessPermissions.ReadWriteExecute)]
-    public bool GraceFrame = false;
-
-    /// <description>
     ///     Время до обновления состояния
     /// </description>
     [ViewVariables(VVAccess.ReadOnly)]
