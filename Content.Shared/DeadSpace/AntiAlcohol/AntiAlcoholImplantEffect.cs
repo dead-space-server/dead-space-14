56bffce28b151c576f59d9a565849f0e56575b68
ZweiL <damirstorozhuk@gmail.com>
2025-11-12 17:09:48 +0200

    modified:   Content.Server/DeadSpace/AntiAlcohol/AlcoTesterComponent.cs modified:   Content.Server/DeadSpace/AntiAlcohol/AlcoTesterSystem.cs modified:   Content.Server/DeadSpace/AntiAlcohol/AntiAlcoholSystem.cs modified:   Content.Server/DeadSpace/AntiAlcohol/AntiAlcoholWatcherComponent.cs modified:   Content.Shared/DeadSpace/AntiAlcohol/AlcoTesterDoAfter.cs modified:   Content.Shared/DeadSpace/AntiAlcohol/AntiAlcoholImplantEffect.cs

--- Content.Shared/DeadSpace/AntiAlcohol/AntiAlcoholImplantEffect.cs
+++ Content.Shared/DeadSpace/AntiAlcohol/AntiAlcoholImplantEffect.cs
@@ -1,3 +1,4 @@
+// Мёртвый Космос, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/dead-space-server/space-station-14-fobos/master/LICENSE.TXT
 using Content.Shared.EntityEffects;
 using Robust.Shared.Prototypes;
 
@@ -5,7 +6,7 @@ namespace Content.Shared.DeadSpace.AntiAlcohol;
 
 /// <summary>
 ///     Простая метка для метаболизма спирта: когда эффект срабатывает,
-///     серверный антиалкогольный имплант получает событие с <see cref="EntityEffectReagentArgs"/> 
+///     серверный антиалкогольный имплант получает событие с <see cref="EntityEffectReagentArgs"/>
 /// </summary>
 public sealed partial class AntiAlcoholImplantEffect : EventEntityEffect<AntiAlcoholImplantEffect>
 {
