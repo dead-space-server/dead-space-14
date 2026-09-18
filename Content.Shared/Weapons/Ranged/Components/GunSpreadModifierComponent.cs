68673e6b35223dfa59c22c29c5d82f43a1027523
TheZuTi <daniil9426@gmail.com>
2026-09-18 22:31:07 +0300

    drobovik

--- Content.Shared/Weapons/Ranged/Components/GunSpreadModifierComponent.cs
+++ Content.Shared/Weapons/Ranged/Components/GunSpreadModifierComponent.cs
@@ -13,4 +13,13 @@ public sealed partial class GunSpreadModifierComponent : Component
     /// </summary>
     [DataField, AutoNetworkedField]
     public float Spread = 1;
+
+    // DS14-start
+    /// <summary>
+    /// A scalar value used instead of <see cref="Spread"/> while the weapon is wielded.
+    /// If not specified, <see cref="Spread"/> is used in both wielded and unwielded states.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public float? WieldedSpread;
+    // DS14-end
 }
